e77167e94560a3f65cf04c50a0205d178cae6627
Kaltum <kaltum.viveros@gmail.com>
2025-09-09 10:05:39 -0600

    configuración inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeScreenComponent } from './screens/home-screen/home-screen.component';
+import { LoginScreenComponent } from './screens/login-screen/login-screen.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//Angular Material Modules
+import {MatButtonModule} from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeScreenComponent,
+    LoginScreenComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
